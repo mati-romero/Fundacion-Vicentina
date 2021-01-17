a301c89881558173208c96248e36d6d7e141599b
mati romero <matias.1993.romero@gmail.com>
2021-01-16 23:29:40 -0300

    Quita de banderas

--- js/colaboradores.js
+++ js/colaboradores.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
 	function desfilar() {
 		colaboradores.animate({
-			marginLeft: '-200px'
+			marginLeft: '-150px'
 		},1000,function() {
 			$('#colaboradores .colaborador:first').insertAfter('#colaboradores .colaborador:last');
         	colaboradores.css('margin-left', '0');

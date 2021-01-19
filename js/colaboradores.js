56c768f1419bd8dbe64c2e918c4d26a4e42ba1da
mati romero <matias.1993.romero@gmail.com>
2021-01-19 18:32:46 -0300

    Cambio de iconos

--- js/colaboradores.js
+++ js/colaboradores.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
 
-	
-
 	var colaboradores = $('#colaboradores');
+	var marginL = "-" + ($('#colaboradores .colaborador:first').width() + 50).toString() + "px";
 
 	function desfilar() {
 		colaboradores.animate({
-			marginLeft: '-139px'
+			marginLeft: marginL
 		},1000,function() {
 			$('#colaboradores .colaborador:first').insertAfter('#colaboradores .colaborador:last');
         	colaboradores.css('margin-left', '0px');

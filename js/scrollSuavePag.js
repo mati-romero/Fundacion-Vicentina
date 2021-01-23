d93043f887d452e6547db90041ffdf3170fbc762
mati romero <matias.1993.romero@gmail.com>
2021-01-23 18:09:41 -0300

    Boton para subir arriba

---
+++ js/scrollSuavePag.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+  // Mostrar y ocultar botón "Volver arriba"
+
+  var btnVolverArriba = $('#btnVolverArriba');
+
+  $(window).on('scroll', function () {
+
+    var mainOffsetTop = $('#Main').offset().top;
+
+    if ($(window).scrollTop() >= mainOffsetTop) {
+
+      btnVolverArriba.css('margin-right', 0);
+
+    }else if ($(window).scrollTop() <= mainOffsetTop/2) {
+
+      btnVolverArriba.css('margin-right', '-60px');
+
+    }
+
+  })
+
+  // Movimiento suave de scroll de "Inicio" y "Volver arriba"
+
+  $('a.volver-arriba').on('click', function (e) {
+    e.preventDefault();
+
+    if ($(window).scrollTop() != 0) {
+      $('html, body').stop().animate({scrollTop: 0}, 1000);
+    }
+
+  })
+
+  // Movimiento suave de scroll de los demás items del menú principal
+  $('a.scroll-suave').on('click', function (e) {
+    e.preventDefault();
+    var seccionOffsetTop = $($(this).attr('href')).offset().top
+    $('html, body').stop().animate({scrollTop: seccionOffsetTop}, 1000);
+  });
+
+});
